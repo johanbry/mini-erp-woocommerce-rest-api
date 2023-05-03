8359bd25da66a561b87a967d53d9d3be455ea66f
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-05-03 12:35:07 +0200

    class Product added

--- classes.php
+++ classes.php
@@ -16,6 +16,27 @@ class Post
     }
 }
 
+class Product
+{
+    public $id;
+    public $permalink;
+    public $title;
+    public $thumb_url;
+    public $price;
+    public $category;
+
+
+    public function __construct($id, $title, $permalink, $thumb_url, $price, $category)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->permalink = $permalink;
+        $this->thumb_url = $thumb_url;
+        $this->price = $price;
+        $this->category = $category;
+    }
+}
+
 class Order
 {
     public $id;
