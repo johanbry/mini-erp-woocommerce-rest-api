9ad42a791b598dbac8a0ba64010522feb7231932
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-05-03 12:36:26 +0200

    divided header content part in template file

---
+++ content_header.php
@@ -0,0 +1,10 @@
+<header>
+    <h1>Mini ERP</h1>
+    <ul>
+        <li><a href="posts.php">Posts</a></li>
+        <li><a href="produkter.php">Produkter</a></li>
+        <li><a href="bestallningar.php">Beställningar</a></li>
+        <li><a href="media.php">Media</a></li>
+    </ul>
+    <button><a href="?sync=true">Synka nu!</a></button>
+</header>
