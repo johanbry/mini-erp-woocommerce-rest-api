1dfd2080d224469f12c41881128d1f0dd2da0e20
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-05-04 01:57:25 +0200

    added headings for list

--- produkter.php
+++ produkter.php
@@ -9,6 +9,20 @@ require __DIR__ . '/header.php';
     <main>
         <h1>Produkter</h1>
 
+        <div class="product-headings">
+            <div class="img-div">
+                <h4>Bild</h4>
+            </div>
+            <div class="title-div">
+                <h4>Namn</h4>
+            </div>
+            <div class="category-div">
+                <h4>Kategori</h4>
+            </div>
+            <div class="price-div">
+                <h4>Pris</h4>
+            </div>
+        </div>
         <div id="content-products">
 
         </div>
