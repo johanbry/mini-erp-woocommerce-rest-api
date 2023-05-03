ccbbb461435a20d841f7aeaf2929e86a9f67fff8
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-05-04 01:56:35 +0200

    syncing of orders and media

--- sync.php
+++ sync.php
@@ -34,6 +34,21 @@ for ($i = 0; $i < count((array)$posts); $i++) {
 
 $json_posts = json_encode($posts_mod);
 
+try {
+  // Array of response results.
+  $media = $wordpress->get('media', array('per_page' => '6'));
+} catch (HttpClientException $e) {
+  echo '<pre><code>' . print_r($e->getMessage(), true) . '</code><pre>'; // Error message.
+}
+
+$media_mod = array();
+
+for ($i = 0; $i < count((array)$media); $i++) {
+  $media_mod[$i] = new Media($media[$i]->id, $media[$i]->alt_text, $media[$i]->media_details->sizes->thumbnail->source_url, $media[$i]->media_details->sizes->full->source_url);
+}
+
+$json_media = json_encode($media_mod);
+
 
 
 $woocommerce = new Client(
@@ -63,9 +78,29 @@ for ($i = 0; $i < count((array)$products); $i++) {
 
 $json_products = json_encode($products_mod);
 
+
+try {
+  // Array of response results.
+  $orders = $woocommerce->get('orders');
+} catch (HttpClientException $e) {
+  echo '<pre><code>' . print_r($e->getMessage(), true) . '</code><pre>'; // Error message.
+  echo '<pre><code>' . print_r($e->getRequest(), true) . '</code><pre>'; // Last request data.
+  echo '<pre><code>' . print_r($e->getResponse(), true) . '</code><pre>'; // Last response data.
+}
+
+$orders_mod = array();
+
+for ($i = 0; $i < count((array)$orders); $i++) {
+  $orders_mod[$i] = new Order($orders[$i]->id, $orders[$i]->status, $orders[$i]->total, $orders[$i]->date_created);
+}
+
+$json_orders = json_encode($orders_mod);
+
 ?>
 
 <script type="text/javascript">
   localStorage.setItem("posts", '<?php echo $json_posts ?>');
+  localStorage.setItem("media", '<?php echo $json_media ?>');
   localStorage.setItem("products", '<?php echo $json_products ?>');
+  localStorage.setItem("orders", '<?php echo $json_orders ?>');
 </script>
