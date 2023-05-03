224948ef8fd9c48b5fbb33cf032ba8a639d126d4
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-05-04 01:55:20 +0200

    media page structure

---
+++ media.php
@@ -0,0 +1,19 @@
+<?php
+require __DIR__ . '/header.php';
+?>
+
+<body onload="renderMedia();">
+    <?php
+    require __DIR__ . '/content_header.php';
+    ?>
+    <main>
+        <h1>Media</h1>
+
+        <div id="content-media">
+
+        </div>
+    </main>
+
+</body>
+
+</html>
