2769c3ad1de6e705d3ff17ae94534bf8fad6dd17
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-05-03 12:35:39 +0200

    product page created

---
+++ produkter.php
@@ -0,0 +1,19 @@
+<?php
+require __DIR__ . '/header.php';
+?>
+
+<body onload="renderProducts();">
+    <?php
+    require __DIR__ . '/content_header.php';
+    ?>
+    <main>
+        <h1>Produkter</h1>
+
+        <div id="content-products">
+
+        </div>
+    </main>
+
+</body>
+
+</html>
